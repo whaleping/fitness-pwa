c7c4a07993b609ac9757d37bc22b29cd37cdc926
whaleping <whale794006@gmail.com>
2026-04-13 19:18:25 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
+const CACHE_NAME = 'fitness-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+// 安裝時儲存靜態檔案
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+});
+
+// 當發出請求時，優先使用快取的內容
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
 
